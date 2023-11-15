cefcc1486c9e7c99ee2984cc933bfab8e17013c6
Cindy Sun <csun76@smith.edu>
2023-11-15 15:43:49 -0500

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Metadata for all xkcd comics (thus far)
+#'
+#' @format A data frame with 2850 rows and 12 columns
+#' \describe{
+#'    \item{num}{Comic number (minimum 1, maximum 2850)}
+#'    \item{year}{Year comic was published}
+#'    \item{month}{Month the comic was published}
+#'    \item{day}{Day the comic was published}
+#'    \item{link}{URL linking to revelant context, source information or large version of comic}
+#'    \item{news}{Seemingly random information about the comic, like a backstory, contributors, mechandise link. May contain raw HTML.}
+#'    \item{title}{Title of the comic}
+#'    \item{safe_title}{We still don't now what this is safe for, Embedding maybe?}
+#'    \item{transcript}{Transcription of the text in the comic}
+#'    \item{alt}{Alt text for the image in web browser}
+#'    \item{img}{URL for the comic image itself}
+#' }
+#' @source <https://xkcd.com/json.html>
+"xkcd_data"
